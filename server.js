cbf56c35c9a75dfa28b9fbf3356d2d8c6b7bbe53
David Smith <dave@dave-smith.co.uk>
2022-01-18 11:53:50 +0000

    Removed redundant routes

--- server.js
+++ server.js
@@ -21,75 +21,8 @@ app.get('/', function (req, res) {
     res.render('index.ejs', {});
 });
 
-app.get('/league/:league', function (req, res) {
-    const league = req.params.league;
-    res.render('league.ejs', {
-        league: league
-    });
-});
-
-app.get('/submit/:league', function (req, res) {
-    const league = req.params.league;
-    res.render('submit.ejs', {
-        league: league,
-    });
-});
-
-app.get('/times/:league', function (req, res) {
-    const league = req.params.league;
-    const options = {
-        uri: apiurl,
-        qs: {
-            league: league,
-        }
-    };
-    request(options, function (err, response, body) {
-        if (response.statusCode < 200 || response.statusCode > 399) {
-            return res.status(response.statusCode).json({});
-        }
-        return res.status(200).json(JSON.parse(body));
-    })
-});
-
-app.post('/times', function (req, res) {
-    console.log('times req : ', req.body);
-
-    let options = {
-        uri: apiurl,
-        method: 'POST',
-        body: req.body,
-        json: true,
-    };
-    request.post(options, function (err, response, body) {
-        console.log('times res : ', body);
-
-        io.sockets.emit('league', 'reload');
-        if (response.statusCode < 200 || response.statusCode > 399) {
-            return res.status(response.statusCode).json({});
-        }
-        return res.status(200).json({});
-    })
-});
-
 app.get('/timer', function (req, res) {
     res.render('timer.ejs', {});
-});
-
-app.get('/timer/:name', function (req, res) {
-    const name = req.params.name;
-    io.sockets.emit('timer', `${name}`);
-    return res.status(200).json({
-        result: true,
-        timer: name,
-    });
-});
-
-app.get('/reward', function (req, res) {
-    console.log('reward req : ', req.body);
-
-    return res.status(200).json({
-        result: true,
-    });
 });
 
 // sockets
